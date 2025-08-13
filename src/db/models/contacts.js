8f5b0414f357143a0e788b5b8f17f6418fcba07f
Andrii <semtrololo@gmail.com>
2025-08-14 01:20:24 +0200

    corrected the mentor's remark

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -1,27 +1,33 @@
 import { Schema, model } from 'mongoose';
 
-const contactsSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
   },
-  phoneNumber: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  email: {
-    type: String,
-  },
-  isFavorite: {
-    type: Boolean,
-    default: false,
-  },
-  contactType: {
-    type: String,
-    required: true,
-    enum: ['work', 'home', 'personal'],
-    default: 'personal',
-  },
-});
+);
 
 export const ContactsCollection = model('contacts', contactsSchema);
