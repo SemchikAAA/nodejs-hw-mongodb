ce42632be26b93325a7eb336737506b81bcf81d8
Andrii <semtrololo@gmail.com>
2025-08-23 13:18:17 +0200

    step 4

---
+++ src/utils/parseSortParams.js
@@ -0,0 +1,24 @@
+import { SORT_ORDER } from '../constants/index.js';
+
+const parseSortOrder = (sortOrder) => {
+  const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
+  if (isKnownOrder) return sortOrder;
+  return SORT_ORDER.ASC;
+};
+
+const parseSortBy = (sortBy) => {
+  if (sortBy === 'name') return sortBy;
+  return 'name';
+};
+
+export const parseSortParams = (query) => {
+  const { sortBy, sortOrder } = query;
+
+  const parsedSortBy = parseSortBy(sortBy);
+  const parsedSortOrder = parseSortOrder(sortOrder);
+
+  return {
+    sortOrder: parsedSortOrder,
+    sortBy: parsedSortBy,
+  };
+};
