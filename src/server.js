76885f56e21d975cde9dcb10540c8a917bae3d31
Andrii <semtrololo@gmail.com>
2025-09-13 15:31:01 +0200

    hw7

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -28,6 +29,8 @@ export function setupServer() {
   app.use(router);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
 
